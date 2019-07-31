4d88f2935e43f71a7361551f40577e8fbb32fc4b
omerch <omerch35@yahoo.com>
2019-07-31 23:38:58 +0200

    project update

---
+++ listings/choices.py
@@ -0,0 +1,47 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '100000':'$100,000',
+  '200000':'$200,000',
+  '300000':'$300,000',
+  '400000':'$400,000',
+  '500000':'$500,000',
+  '600000':'$600,000',
+  '700000':'$700,000',
+  '800000':'$800,000',
+  '900000':'$900,000',
+  '1000000':'$1M+',
+}
+
+state_choices = {
+        
+       'BW': 'Baden Wurttemberg',
+       'BAV': 'Bavaria' ,
+       'BER': 'Berlin',
+       'BRN':'Brandenburg',
+       'BMN':'Bremen',
+       'HAM':'Hamburg',
+       'HSE':'Hesse',
+       'LS':'Lower Saxony',
+       'MV': 'Mecklenburg Vorpommern',
+       'NRW':'North Rhine Westphalia',
+       'RP': 'Rhineland Palatinate',
+       'SAR': 'Saarland',
+       'SAX': 'Saxony',
+       'SA': 'Saxony-Anhalt',
+       'SWG': 'Schleswig',
+       'SH': 'Schleswig Holstein',
+       'THU': 'Thuringia',
+       
+}
